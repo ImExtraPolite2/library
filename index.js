977b5f6f62b1e4495388c2406d15acc8b98ca190
ImExtraPolite <imextrapolite@gmail.com>
2024-01-29 13:01:56 -0500

    make modal

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const myLibrary = [];
 
+const openModal = () => {
+  const open = document.querySelector('.add-new-book');
+  const modal = document.querySelector('.modal');
+  
+  open.addEventListener('click', () => {
+    modal.showModal();
+  })
+}
+
+const closeModal = () => {
+  const close = document.querySelector('#exit');
+  const modal = document.querySelector('.modal');
+
+  close.addEventListener('click', () => {
+    modal.close();
+  })
+}
+
 function Book(title, author, numOfPages, read) {
   this.title = title;
   this.author = author;
@@ -7,7 +25,12 @@ function Book(title, author, numOfPages, read) {
   this.read = read;
 }
 
-
 function addBookToLibrary() {
+  openModal();
+  closeModal();
+  
   const addBook = document.querySelector('add-new-book');
 }
+
+addBookToLibrary();
+
